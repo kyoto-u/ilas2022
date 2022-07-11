bb5b730cba620c72ff64dcf356e4820199aa3255
powapowa03 <61612576+powapowa03@users.noreply.github.com>
2022-07-11 18:14:27 +0900

    合体させました！

--- ComfortablePandA_test.py
+++ ComfortablePandA_test.py
@@ -6,6 +6,26 @@ import requests
 url = "http://127.0.0.1:8000"
 
 #JSON形式のデータ
-payload = {'some': 'data'}
+payload = [
+    "userid",
+    [ # infomation_list
+        {
+            "course": {
+                "id": "授業ID",
+                "name": "名前",
+                "link": "https://panda.ecs.kyoto-u.ac.jp/portal/site/授業ID"
+            },
+            "entries": [
+                {
+                    "id": "課題ID",
+                    "title": "第九回の課題",
+                    "dueTime": "期限日",
+                    "closeTime": "遅延提出期限日",
+                    "hasFinished": "終わったかどうか" #bool　値
+                }
+            ],
+            "isRead": False  # false ← わからない???
+        },
+    ]]
 r = requests.post(url, json=payload)
 print(r)
--- test.py
+++ test.py
@@ -1,10 +1,30 @@
 from flask import Flask, request
 import json
-
+from html import entities
+import pickle
 
 #def system_run内でSlack側の処理を記述
-def system_run(data):
-    print(data["some"])
+def system_run(infList):
+    # 整形
+    usedInfList = [infList[0]] # 保存するようの情報
+    # usedInfListに必要な情報を追加していく
+    for inf in infList[1]:
+
+        usedInf = {
+            "course" : inf["course"]["name"], # 授業名
+            "title" : inf["entries"][0]["title"], # 課題名]
+            "dueTime" : inf["entries"][0]["dueTime"], # 締切日時
+            "closeTime" : inf["entries"][0]["closeTime"], # 遅延提出期限日
+            "hasFinished" : inf["entries"][0]["hasFinished"], # 終わったかどうか(bool値)
+            "isRead" : inf["isRead"] # 何に使うかは7/4現在不明(bool値)
+        }
+
+        usedInfList.append(usedInf)
+    
+    # 保存
+    with open('datas.pickle', mode='wb') as f:
+        pickle.dump(usedInfList, f)
+    print(usedInfList)
 
 
 #以下、Flask関連の処理
