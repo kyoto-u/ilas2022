c51ce599948947db8f2caf6e4679b65d09aedf8c
juglans <r.juglans.1018@gmail.com>
2022-07-15 15:49:09 +0900

    [fix]課題がない場合エラーとなる現象を修正

--- save.py
+++ save.py
@@ -8,17 +8,18 @@ import pickle
 def system_run(infList):
     # 整形
     PandaAssignments = []
+    PandaAssignment={}
     # usedInfListに必要な情報を追加していく
     for inf in infList[1]:
-
-        PandaAssignment = {
-            "course" : inf["course"]["name"], # 授業名
-            "title" : inf["entries"][0]["title"], # 課題名]
-            "dueTime" : inf["entries"][0]["dueTime"], # 締切日時
-            "closeTime" : inf["entries"][0]["closeTime"], # 遅延提出期限日
-            "hasFinished" : inf["entries"][0]["hasFinished"], # 終わったかどうか(bool値)
-            "isRead" : inf["isRead"] # 何に使うかは7/4現在不明(bool値)
-        }
+        if (inf['entries'] != []):
+            PandaAssignment = {
+                "course" : inf["course"]["name"], # 授業名
+                "title" : inf["entries"][0]["title"], # 課題名]
+                "dueTime" : inf["entries"][0]["dueTime"], # 締切日時
+                "closeTime" : inf["entries"][0]["closeTime"], # 遅延提出期限日
+                "hasFinished" : inf["entries"][0]["hasFinished"], # 終わったかどうか(bool値)
+                "isRead" : inf["isRead"] # 何に使うかは7/4現在不明(bool値)
+            }
 
         PandaAssignments.append(PandaAssignment)
     
@@ -35,7 +36,6 @@ app = Flask(__name__, static_folder='.', static_url_path='')
 @app.route('/', methods=["POST"])
 def receiver_run():
     param = json.loads(request.data.decode('utf-8'))
-    
     system_run(param)
 
     return "Success!"
