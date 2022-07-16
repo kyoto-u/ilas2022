f17d00e292d796b15d0c14ef90da21d76351257b
Koki Takeda <kouki.0509.takeda@gmail.com>
2022-07-16 11:44:36 +0000

    不要部分の削除

--- src/browser_extention/src/content_script.ts
+++ src/browser_extention/src/content_script.ts
@@ -1,17 +1,14 @@
 import { saveHostName } from "./features/storage";
-import { createMiniSakai, addMiniSakaiBtn } from "./minisakai";
-import { isLoggedIn, miniSakaiReady } from "./utils";
+import { createMiniSakai } from "./minisakai";
+import { isLoggedIn } from "./utils";
 
 /**
  * Creates miniSakai.
  */
 async function main() {
     if (isLoggedIn()) {
-        addMiniSakaiBtn();
         const hostname = window.location.hostname;
         createMiniSakai(hostname);
-
-        miniSakaiReady();
         await saveHostName(hostname);
     }
 }
